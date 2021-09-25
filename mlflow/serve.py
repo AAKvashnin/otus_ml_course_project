7aa5a9361f70a004c14c527f5c9cf018ed6786be
AAKvashnin <alexey.kvashnin@gmail.com>
2021-09-25 20:32:40 +0300

    add register model

--- mlflow/serve.py
+++ mlflow/serve.py
@@ -1,4 +1,15 @@
 import mlflow
 import mlflow.spark
 
+
+from pyspark.sql import SparkSession
+
+spark = (SparkSession.builder \
+                .config("spark.hadoop.yarn.timeline-service.enabled","false") \
+                .config("spark.submit.deployMode","client") \
+                .config("spark.jars","/home/alexey_kvashnin/gcs-connector-hadoop3-2.2.2.jar") \
+                .config("spark.executor.instances","10") \
+                .master("yarn") \
+                .getOrCreate())
+
 model=mlflow.spark.load_model("models:/Complaint classification model/1");
