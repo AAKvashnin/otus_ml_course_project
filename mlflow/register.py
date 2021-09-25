fba87c9de87897d64ebb23dc6a72708fe6c25aa3
AAKvashnin <alexey.kvashnin@gmail.com>
2021-09-25 19:31:20 +0300

    add register model

--- mlflow/register.py
+++ mlflow/register.py
@@ -5,6 +5,6 @@ df = mlflow.search_runs(0, order_by=["metrics.f1_weighted DESC"])
 print(df)
 run_id=df.loc[df['metrics.f1_weighted'].idxmax()]['run_id']
 
-model_uri = mlflow.get_artifact_uri() + "/0/" + run_id + "/model")
+model_uri = mlflow.get_artifact_uri() + "/0/" + run_id + "/model"
 mlflow.register_model(model_uri,"Complaint classification model")
 
