b5c69c577632fb404842388f4f62b60db75173ea
AAKvashnin <alexey.kvashnin@gmail.com>
2021-09-26 10:18:33 +0300

    added index to string

--- scripts/serve_online.sh
+++ scripts/serve_online.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 gcloud auth login
 export MLFLOW_TRACKING_URI=http://localhost:8000
-mlflow models serve -m "models:/Complaint classification model/1"
+mlflow models serve -m "models:/Complaint classification model/1" -h 0.0.0.0
