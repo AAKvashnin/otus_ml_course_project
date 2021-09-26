b980a0e2d589ce6791f06b5b15afda03f14f0a27
AAKvashnin <alexey.kvashnin@gmail.com>
2021-09-26 17:23:44 +0300

    added validate step

--- scripts/predict_offline.sh
+++ scripts/predict_offline.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 gcloud auth login
 export MLFLOW_TRACKING_URI=http://localhost:8000
-mlflow run git@github.com:AAKvashnin/otus_ml_course_project.git -e predict -P --test-data=/home/alexey_kvashnin/inputData.parquet --result-data=/home/alexey_kvashnin/outputData.parquet
+mlflow run git@github.com:AAKvashnin/otus_ml_course_project.git -e predict -P test_data=/home/alexey_kvashnin/inputData.parquet -P result_data=/home/alexey_kvashnin/outputData.parquet
